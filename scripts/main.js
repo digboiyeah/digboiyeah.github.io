2463eac0f33708b4e5f772e6574fbef6d2f8c958
digboiyeah <84730133+digboiyeah@users.noreply.github.com>
2025-11-24 00:31:24 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/MWKA.png") {
+    myImage.setAttribute("src", "images/MWKA2.png");
+  } else {
+    myImage.setAttribute("src", "images/MWKA.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
+
+function setUserName() {
+    const myName = prompt("Please enter name");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `this is, ${myName}, website`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `You are cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () =>{
+    setUserName();
+})
